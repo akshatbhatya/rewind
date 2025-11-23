21a0d9ac9981831b629c04881ba5bae657936ed8
akshatbhatya <akshatbhatya@gmail.com>
2025-11-23 23:13:44 +0530

    loops

---
+++ php/loops.php
@@ -0,0 +1,26 @@
+<?php
+
+/** loops */
+
+for($num=1;$num<=10;$num++){
+    echo $num ."\n";
+}
+
+
+
+
+$num=[1,2,3,4,5,6,7];
+
+foreach($num as $n){
+    echo $n ."\n";
+
+}
+
+
+
+$i=1;
+while($i<=20){
+    echo $i ."\n";
+    $i++;
+
+}
