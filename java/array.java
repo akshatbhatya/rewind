4ab9503892a2898d55fd5eeff30dd513fc3b18a6
akshatbhatya <akshatbhatya@gmail.com>
2026-01-05 23:22:45 +0530

    feat: Comment out unused array initialization and related code in main method

--- java/array.java
+++ java/array.java
@@ -2,16 +2,29 @@ public class array {
     
     public static void main(String[] args){
 
-        int[] arr={1,2,3,4,5,6};
+        // int[] arr={1,2,3,4,5,6};
 
-        int sum[]={};
-        int index= 0;
-        for (int i : arr) {
+        // int sum[]={};
+        // int index= 0;
+        // for (int i : arr) {
+        //     System.out.println(i);
+        //     // sum[index]=i;
+        //     i++;
+        // }
+
+        // System.out.println(sum);
+
+
+        int i=0;
+        for(;;){
+            if(i>10)break;
             System.out.println(i);
-            sum[index]=i;
             i++;
         }
 
-        System.out.println(sum);
+        for(int j=0;j<=10;j++){
+            System.out.println(j);
+
+        }
     }
 }
