6c2ac112a5d6f21979c430d1fe1c0e3a4d9ea3c1
akshatbhatya <akshatbhatya@gmail.com>
2026-01-14 20:37:19 +0530

    feat: Add Demo and B classes with constructors and main method demonstration

---
+++ java/A.java
@@ -0,0 +1,34 @@
+class Demo{
+
+    public Demo(){
+        super();
+        System.out.println(" class demo clalled");
+    }
+
+    public Demo(int num) {
+       System.out.println(num);
+    }
+}
+
+
+class B extends Demo{
+    public B(){
+        super();
+        System.out.println("b is called");
+    }
+
+    public B(int num){
+        super(num);
+        System.out.println("num is : "+num);
+    }
+}
+
+
+public class A {
+    public static void main(String[] args){
+        B b1=new B();
+        B b2 = new B(5);
+        // System.out.print(b1);
+    }
+}
+
