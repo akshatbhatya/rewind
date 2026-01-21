120166bf20345aac97f8a3b1b7dfc365cee7f248
akshatbhatya <akshatbhatya@gmail.com>
2026-01-21 14:13:41 +0530

    feat: Add advanceClass and advanceClass2 for multiplication and division operations; implement CaclulatorCls for addition, subtraction, and method demonstration in main

---
+++ java/inheritanceLearn/advCalc.java
@@ -0,0 +1,10 @@
+package inheritanceLearn;
+
+class advanceClass extends advanceClass2{
+    
+    public int multi(int a, int b){
+        return a * b;
+    }
+    
+}
+
---
+++ java/inheritanceLearn/advCalc2.java
@@ -0,0 +1,8 @@
+package inheritanceLearn;
+
+class advanceClass2{
+    public int divide(int a, int b){
+            return a/b;
+        }
+}
+
