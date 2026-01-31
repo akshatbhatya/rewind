92ad74f6a0cf4628cf9968fe966986a4d0a6176e
akshatbhatya <akshatbhatya@gmail.com>
2026-01-31 23:18:46 +0530

    feat: Implement Car abstraction with Suzuki and UpdatedSuzuki classes; add main method demonstration

---
+++ java/abstraction/Main.java
@@ -0,0 +1,41 @@
+package abstraction;
+
+abstract class Car{
+
+    public abstract void startCar();
+    public abstract void engine();
+
+    public void exhaustSound(){
+        System.out.println("brom brom");
+    }
+}
+
+
+abstract class Suzuki extends Car{
+
+    public void startCar(){
+        System.out.println("car suzuki started");
+    }
+}
+
+class UpdatedSuzuki extends Suzuki{
+
+@Override 
+public void engine(){
+    System.out.println("1200cc");
+}
+
+}
+
+public class Main {
+
+   public static void main(String[] args) {
+        UpdatedSuzuki suzuki =new UpdatedSuzuki();
+        suzuki.exhaustSound();
+        suzuki.startCar();
+        suzuki.engine();
+   }
+
+
+    
+}
