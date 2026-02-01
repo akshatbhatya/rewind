2e08aa691450ca0d253f6b9465726be2a7ebd0e9
akshatbhatya <akshatbhatya@gmail.com>
2026-02-01 23:15:57 +0530

    interfaces

---
+++ java/interfaces/Main.java
@@ -0,0 +1,34 @@
+package interfaces;
+
+interface A{
+
+   void show();
+   void configs();
+}
+
+interface B extends A{
+}
+
+class C implements A{
+   
+   @Override
+   public  void show() {
+       System.out.println("show");
+   }
+
+ 
+   
+   @Override
+   public void configs(){
+      System.out.println("configs");
+
+   }
+}
+public class Main {
+
+   public static void main(String[] args){
+      C c= new C();
+      c.configs();
+   }
+    
+}
